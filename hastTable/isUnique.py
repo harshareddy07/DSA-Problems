47640554d8a90d8bd0306fc852df79eac7fc8050
harshareddy07 <56046182+harshareddy07@users.noreply.github.com>
2024-07-28 03:29:56 +0530

    Update isUnique.py

--- hastTable/isUnique.py
+++ hastTable/isUnique.py
@@ -1,4 +1,4 @@
-#Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures? 
+# Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures? 
 
 def is_unique_chars(s):
     checker = 0
@@ -12,29 +12,29 @@ def is_unique_chars(s):
 
 # Explanation
 
-Sample Execution for "abac"
-
-Initial Value:
-checker = 0 (binary 00000000).
-
-Processing Each Character:
-
-Character 'a':
-
-val = 'a' - 'a' = 0
-Check if bit 0 is set: checker & (1 << 0) = 0 & 1 = 0 (not set)
-Update checker: checker |= (1 << 0) = 0 | 1 = 1 (binary 00000001)
-Character 'b':
-
-val = 'b' - 'a' = 1
-Check if bit 1 is set: checker & (1 << 1) = 1 & 2 = 0 (not set)
-Update checker: checker |= (1 << 1) = 1 | 2 = 3 (binary 00000011)
-Character 'a':
-
-val = 'a' - 'a' = 0
-Check if bit 0 is set: checker & (1 << 0) = 3 & 1 = 1 (bit 0 is set)
-Since bit 0 is set, it means 'a' has been seen before, so the function returns false.
-Character 'c':
-
-This character is not processed because the function already returned false upon 
-detecting the duplicate 'a'.
+    # Sample Execution for "abac"
+    
+    # Initial Value:
+    # checker = 0 (binary 00000000).
+    
+    # Processing Each Character:
+    
+    # Character 'a':
+    
+    # val = 'a' - 'a' = 0
+    # Check if bit 0 is set: checker & (1 << 0) = 0 & 1 = 0 (not set)
+    # Update checker: checker |= (1 << 0) = 0 | 1 = 1 (binary 00000001)
+    # Character 'b':
+    
+    # val = 'b' - 'a' = 1
+    # Check if bit 1 is set: checker & (1 << 1) = 1 & 2 = 0 (not set)
+    # Update checker: checker |= (1 << 1) = 1 | 2 = 3 (binary 00000011)
+    # Character 'a':
+    
+    # val = 'a' - 'a' = 0
+    # Check if bit 0 is set: checker & (1 << 0) = 3 & 1 = 1 (bit 0 is set)
+    # Since bit 0 is set, it means 'a' has been seen before, so the function returns false.
+    # Character 'c':
+    
+    # This character is not processed because the function already returned false upon 
+    # detecting the duplicate 'a'.
