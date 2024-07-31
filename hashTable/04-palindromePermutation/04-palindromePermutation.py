4011944da87e2bc7a3c664a223fb9d4358107c91
harshareddy07 <56046182+harshareddy07@users.noreply.github.com>
2024-08-01 00:05:22 +0530

    Update 04-palindromePermutation.py

--- hashTable/04-palindromePermutation/04-palindromePermutation.py
+++ hashTable/04-palindromePermutation/04-palindromePermutation.py
@@ -34,5 +34,34 @@ def palindromePermutation(phrase) :
 
 v = palindromePermutation("tacos atc")
 print("v", v) 
-    
-    
+
+
+
+// solution 2
+
+
+def get_char_number(c: str) -> int:
+    if 'a' <= c <= 'z':
+        return ord(c) - ord('a')
+    elif 'A' <= c <= 'Z':
+        return ord(c.lower()) - ord('a')
+    return -1
+
+def buildObject(phrase):
+    print("v", ord('z'))
+    table = [0] * 26
+    countOdd = 0
+    for index,c in enumerate(phrase):
+        x = get_char_number(c)
+        if x != -1:
+            table[x] += 1
+            if table[x] % 2 == 1:
+                countOdd+= 1
+            else:
+                countOdd-= 1
+    return countOdd <= 1
+
+
+def palindromePermutation(phrase) :
+    return buildObject(phrase)
+
