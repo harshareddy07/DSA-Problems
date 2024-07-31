3914a05ff50ca30f257536a9967ff8c125027499
harshareddy07 <56046182+harshareddy07@users.noreply.github.com>
2024-07-31 23:59:32 +0530

    Create 04-palindromePermutation.py

---
+++ hashTable/04-palindromePermutation/04-palindromePermutation.py
@@ -0,0 +1,38 @@
+def get_char_number(c: str) -> int:
+    if 'a' <= c <= 'z':
+        return ord(c) - ord('a')
+    elif 'A' <= c <= 'Z':
+        return ord(c.lower()) - ord('a')
+    return -1
+
+def buildObject(phrase):
+    print("v", ord('z'))
+    table = [0] * 26
+    for index,c in enumerate(phrase):
+        x = get_char_number(c)
+        if x != -1:
+            table[x] += 1
+    return table
+
+
+def checkPalindrome(x):
+    alreadyOddExist = False
+    for index, j in enumerate(x):
+        print("x", j)
+        if j%2==1:
+            if alreadyOddExist:
+                return False
+            alreadyOddExist = True
+    return True
+            
+
+def palindromePermutation(phrase) :
+    c_table = buildObject(phrase)
+    print("c",c_table)
+    return checkPalindrome(c_table)
+
+
+v = palindromePermutation("tacos atc")
+print("v", v) 
+    
+    
