895856a314451ebec818af198b652c4eeb24cd34
harshareddy07 <56046182+harshareddy07@users.noreply.github.com>
2024-11-06 22:40:47 +0530

    Create main.py

---
+++ Basic/binary-search/main.py
@@ -0,0 +1,23 @@
+target_pos = -1
+
+def find_target(arr, n):
+    l = 0 
+    r = len(arr) - 1
+    while l <= r:
+        mid = (l + r) // 2
+        if arr[mid] == n:
+            globals() ['target_pos'] = mid
+            return True
+        else:
+          if arr[mid] < n:
+            l = mid
+          else:
+            r = mid
+        
+    return False
+    
+arr  = [ 2 , 6, 8, 12, 44, 55 , 77]
+target = 55
+
+result = find_target(arr, target)
+print(f"Target Found at {target_pos}") if result else print("Target Not Found")
