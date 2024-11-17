8509cadb62b72ad01efa3e0de961f81cddef0c35
harshareddy07 <56046182+harshareddy07@users.noreply.github.com>
2024-11-17 18:44:09 +0530

    Update main.py

--- Basic/insertion-sort/main.py
+++ Basic/insertion-sort/main.py
@@ -1,1 +1,17 @@
 
+def sort_list(arr):
+    for i in range(1, len(arr)):
+        j = i - 1
+        i  = i
+        key = arr[i]
+        while arr[j] > key and j >= 0:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr
+        
+arr  = [ 4, 9, 3, 2, 6 ,8, 1]
+
+
+result = sort_list(arr)
+print(f"Sorted List is {arr}") if result else print(" Not Found")
