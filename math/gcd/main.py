fc6fd2cf70e55a050430a9791d8c294c105bd4e4
Harshavardhan <56046182+harshareddy07@users.noreply.github.com>
2025-06-24 20:17:55 +0200

    Update main.py

--- math/gcd/main.py
+++ math/gcd/main.py
@@ -1,12 +1,14 @@
 class Solution:
     def gcd(self, a, b):
-        min_d = min(a,b)
-        res = min_d
-        
+        ## Approach 1
+        res = min(a,b)       
         while res > 0 :
             if a % res == 0 and b % res ==0:
                 break
-                
-            res -= 1
-            
+            res -= 1  
         return res
+        
+        ## Approach 2
+        while b != 0:
+            a, b = b, a % b
+        return a
