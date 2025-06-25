43a081efae46e2a598d022e5b056d231d065cdec
Harshavardhan <56046182+harshareddy07@users.noreply.github.com>
2025-06-25 11:37:16 +0200

    Update main.py

--- math/isPalindrome/main.py
+++ math/isPalindrome/main.py
@@ -16,3 +16,11 @@ class Solution:
 		    r -= 1
             
         return isPalindrome
+
+	def isPalindrome(n):
+	    original = n
+	    rev = 0
+	    while n > 0:
+	        rev = rev * 10 + n % 10
+	        n = n // 10
+	    return rev == original
