e16a71f22ac81e7bc85427b088bab8835ed68602
Harshavardhan <56046182+harshareddy07@users.noreply.github.com>
2025-08-05 15:38:49 +0200

    Update findMissing.py

--- array/findMissing/findMissing.py
+++ array/findMissing/findMissing.py
@@ -4,3 +4,17 @@ class Solution:
         n = len(arr) + 1  # Since one number is missing
         total = n * (n + 1) // 2  # Sum of 1 to n
         return total - sum(arr)
+    def find_missing_custom_range(arr):
+        start = min(arr)
+        end = max(arr)
+        
+        xor_full = 0
+        xor_arr = 0
+    
+        for i in range(start, end + 1):
+            xor_full ^= i
+    
+        for num in arr:
+            xor_arr ^= num
+    
+        return xor_full ^ xor_arr
