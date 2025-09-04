b5b40e1f412eeefdd439e47d5bd740c3e8767084
Harshavardhan <56046182+harshareddy07@users.noreply.github.com>
2025-09-04 12:52:51 +0200

    Update maximum-depth-of-binary-tree.py

--- Tree/maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ Tree/maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -10,8 +10,37 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: int
         """
+        # Using recursive
         if root is None:
             return 0
         
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+        # using bst
+        if root is None:
+            return 0
+        level = 1
+        q = deque([root])
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                level += 1
+        return level
+
+        # using dfs
+
+        stack = [[root, 1]]
+        res = 0
+        while stack:
+            node, depth =  stack.pop()
+            if node:
+                res = max(res, depth)
+                stack.extend([node.left, depth+1])
+                stack.extend([node.right,depth+1])
+
+        return res
         
