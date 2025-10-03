1c6936799b20a5d9a5dc22229c6873a6e66ca203
Harshavardhan <56046182+harshareddy07@users.noreply.github.com>
2025-10-03 15:07:04 +0200

    Update 02-containsDuplicate.py

--- hashTable/02-containsDuplicate/02-containsDuplicate.py
+++ hashTable/02-containsDuplicate/02-containsDuplicate.py
@@ -8,3 +8,11 @@ class Solution:
 
         return False
         
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        visited = set()
+        for num in nums:
+            if num in visited:
+                return True
+            visited.add(num)
+        return False
