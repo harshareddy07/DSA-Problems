a55f2d129589127550be5bb2c6af3749766cadc4
Harshavardhan <56046182+harshareddy07@users.noreply.github.com>
2025-10-13 18:33:08 +0200

    Update ransom-note.py

--- hashTable/ransom-note/ransom-note.py
+++ hashTable/ransom-note/ransom-note.py
@@ -14,3 +14,14 @@ class Solution:
                 countMag[t] -= 1
 
         return True
+
+from collections import Counter
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransom = Counter(ransomNote)
+        mag = Counter(magazine)
+       
+        for key, val in ransom.items():
+            if key not in mag or mag[key] < val:
+                return False
+        return True
